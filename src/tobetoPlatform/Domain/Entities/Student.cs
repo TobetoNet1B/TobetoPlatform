615d0264eb39769a2d95eed6d9db18f5ba0ab02a
Emirhan Çelebi <emirhan@celebi.dev>
2023-12-14 16:15:34 +0300

    Update Student.cs

--- src/tobetoPlatform/Domain/Entities/Student.cs
+++ src/tobetoPlatform/Domain/Entities/Student.cs
@@ -5,12 +5,12 @@ namespace Domain.Entities;
 public class Student : Entity<Guid>
 {
     public int UserId { get; set; }
-    public virtual User User { get; set; } = null!;
     public string? IdentityNumber { get; set; }
     public DateTime? BirthDate { get; set;}
     public string? PhoneNumber { get; set;}
     public string? About { get; set; }
     public string? ImgUrl { get; set; }
+    public virtual User User { get; set; } = null!;
     public virtual Address Address {  get; set; } = null!;
     public virtual ICollection<Ability> Abilities { get; set; } = null!;
     public virtual ICollection<StudentAppeal> StudentAppeals { get; set; } = null!;
