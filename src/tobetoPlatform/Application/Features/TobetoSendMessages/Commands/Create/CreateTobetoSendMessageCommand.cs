df1fc9054895445a36ba00c9b308c9691e2c09ce
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2024-03-31 00:44:28 +0300

    Send Mail Completed

--- src/tobetoPlatform/Application/Features/TobetoSendMessages/Commands/Create/CreateTobetoSendMessageCommand.cs
+++ src/tobetoPlatform/Application/Features/TobetoSendMessages/Commands/Create/CreateTobetoSendMessageCommand.cs
@@ -1,8 +1,13 @@
 using Application.Features.TobetoSendMessages.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Mailing;
+using Core.Security.Entities;
 using Domain.Entities;
 using MediatR;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System.Web;
 
 namespace Application.Features.TobetoSendMessages.Commands.Create;
 
@@ -21,21 +26,46 @@ public class CreateTobetoSendMessageCommand : IRequest<CreatedTobetoSendMessageR
         private readonly ITobetoSendMessageRepository _tobetoSendMessageRepository;
         private readonly TobetoSendMessageBusinessRules _tobetoSendMessageBusinessRules;
 
-        public CreateTobetoSendMessageCommandHandler(IMapper mapper, ITobetoSendMessageRepository tobetoSendMessageRepository,
-                                         TobetoSendMessageBusinessRules tobetoSendMessageBusinessRules)
+
+        private readonly IMailService _mailService;
+        private readonly MailSettings _mailSettings;
+
+        public CreateTobetoSendMessageCommandHandler(IMapper mapper,
+                                                     ITobetoSendMessageRepository tobetoSendMessageRepository,
+                                                     TobetoSendMessageBusinessRules tobetoSendMessageBusinessRules,
+                                                     IMailService mailService,
+                                                     IConfiguration configuration)
         {
             _mapper = mapper;
             _tobetoSendMessageRepository = tobetoSendMessageRepository;
             _tobetoSendMessageBusinessRules = tobetoSendMessageBusinessRules;
+            _mailService = mailService;
+            const string configurationSection = "MailSettings";
+            _mailSettings =
+                configuration.GetSection(configurationSection).Get<MailSettings>()
+                ?? throw new NullReferenceException($"\"{configurationSection}\" section cannot found in configuration.");
         }
 
         public async Task<CreatedTobetoSendMessageResponse> Handle(CreateTobetoSendMessageCommand request, CancellationToken cancellationToken)
         {
+            request.SenderEmail = _mailSettings.SenderEmail;
+            request.SenderName = _mailSettings.SenderFullName;
             TobetoSendMessage tobetoSendMessage = _mapper.Map<TobetoSendMessage>(request);
+
 
             await _tobetoSendMessageRepository.AddAsync(tobetoSendMessage);
 
             CreatedTobetoSendMessageResponse response = _mapper.Map<CreatedTobetoSendMessageResponse>(tobetoSendMessage);
+
+            var toEmailList = new List<MailboxAddress> { new(name: $"{tobetoSendMessage.ReceiverName}", tobetoSendMessage.ReceiverEmail) };
+
+            _mailService.SendMail(
+            new Mail
+            {
+                ToList = toEmailList,
+                Subject = tobetoSendMessage.Subject,
+                TextBody = tobetoSendMessage.Content
+            });
             return response;
         }
     }
--- src/tobetoPlatform/Application/Features/TobetoSendMessages/Commands/Create/CreateTobetoSendMessageCommandValidator.cs
+++ src/tobetoPlatform/Application/Features/TobetoSendMessages/Commands/Create/CreateTobetoSendMessageCommandValidator.cs
@@ -8,8 +8,8 @@ public class CreateTobetoSendMessageCommandValidator : AbstractValidator<CreateT
     {
         RuleFor(c => c.Subject).NotEmpty();
         RuleFor(c => c.Content).NotEmpty();
-        RuleFor(c => c.SenderName).NotEmpty();
-        RuleFor(c => c.SenderEmail).NotEmpty();
+        //RuleFor(c => c.SenderName).NotEmpty();
+        //RuleFor(c => c.SenderEmail).NotEmpty();
         RuleFor(c => c.ReceiverName).NotEmpty();
         RuleFor(c => c.ReceiverEmail).NotEmpty();
     }
